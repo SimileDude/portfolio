5fbf291969050cb80da438389690b2ae7dc73871
ash <ashkherad@gmail.com>
2023-03-18 10:46:15 -0400

    Add base files

---
+++ src/components/LeftPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LeftPanel.scss';
+
+const LeftPanel = () => {
+  return (
+    <div className="left-panel">
+      LeftPanel
+      <div className="box"> test box</div>
+    </div>
+  );
+};
+
+export default LeftPanel;
+
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LeftPanel from '../components/LeftPanel';
+
+const About = () => {
+  return (
+    <>
+      <LeftPanel />
+      jhg
+    </>
+  );
+};
+
+export default About;
+
