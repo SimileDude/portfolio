9d6abc94f5cded9252ad56b9060799c954ae0688
ash <ashkherad@gmail.com>
2023-03-30 13:22:39 -0400

    Add touch ups

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,33 +1,56 @@
 const projects = [
   {
+    index: 0,
     title: 'CryptoHub (personal project)',
     description:
       'A personal fun project. It is a cryptocurrency webapp that allows users to search for a specific cryptocurrency and view its chart and details. All of the code is available on my github.',
     logoLink: 'src/assets/Btc.png',
     link: 'https://github.com/SimileDude/CryptoHub',
+    tools: ['React.js', 'Javascript', 'Chart.js', 'CoinGecko API'],
   },
   {
+    index: 1,
     title: 'AshKherad.com',
     description:
       "I have the code for this portfolio site available on my github for review. It's built, from scratch, with React.js and other tools.",
     link: 'https://google.com',
     logoLink: 'src/assets/ashkherad.png',
+    tools: ['React.js', 'Javascript', 'SASS', 'Figma', 'Notion'],
   },
   {
+    index: 2,
     title: 'Akindi',
     description:
-      'Akindi.com is an EdTech start-up automating assessments for schools and teachers. Currently working here part-time, primarily front-end work and back-end of the platform.',
+      'Akindi.com is an EdTech SAAS start-up automating assessments for schools and teachers. Currently working here part-time, primarily front-end work and back-end of the platform.',
     link: 'https://google.com',
     logoLink:
       'https://uploads-ssl.webflow.com/6127b97b577bef0aa84fcd26/6127b97b577befbc4d4fcd50_A%20mark.svg',
+    tools: [
+      'Vue.js 3',
+      'Vuex',
+      'Bootstrap',
+      'Google Cloud Platform',
+      'Nodejs',
+      'Express',
+    ],
   },
   {
+    index: 3,
     title: 'Gram Cardiology',
     description: `I designed and developed a web application to allow for reporting of various cardiac diagnostic tests 
     (i.e. ECHOs, ECGs, stress tests) with integration into the electronic medical record. After implementation, physician 
     reading time was reduced, allowing for a 20% more diagnostic tests to be performed on a daily basis`,
     logoLink: 'src/assets/gramcardiology.png',
     link: 'https://google.com',
+    tools: [
+      'React.js',
+      'Redux',
+      'Material UI',
+      'Google Cloud Platform',
+      'Node.js',
+      'Express',
+      'Jest',
+    ],
   },
 ];
 
--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import projects from '../../data/projects';
 import './Work.scss';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 const Work = () => {
   return (
     <>
       <h1>Recent Work</h1>
       <ul>
-        {projects.map((project, index) => {
+        {projects.map((project) => {
           return (
-            <li key={index} className="project-item">
+            <li key={project.index} className="project-item">
               <div className="logo-container">
                 {project.logoLink && (
                   <img
@@ -20,19 +21,26 @@ const Work = () => {
                 )}
               </div>
               <section className="project-info">
-                <h2>
-                  <button onClick={() => window.open(project.link)}>
+                <h2 className="project-link">
+                  <button
+                    onClick={() => window.open(project.link)}
+                    className="project-title"
+                  >
                     {project.title}
+                    <HiOutlineArrowNarrowRight className="right-arrow" />
                   </button>
                 </h2>
                 <div className="description-container">
                   <p className="description">{project.description}</p>
-                  <button
-                    onClick={() => window.open(project.link)}
-                    className="ext-link"
-                  >
-                    Go
-                  </button>
+                  <ul className="tools">
+                    {project.tools.map((tool, index) => {
+                      return (
+                        <li key={index} className="tool">
+                          {tool}
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
               </section>
             </li>
